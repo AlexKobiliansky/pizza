f93892f108a5af44a044423793c7ffa0af0b3977
alex <alex_kobilianskiy@bigmir.net>
2020-12-01 13:12:21 +0200

    part 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
 import logoSvg from "../assets/img/pizza-logo.svg";
 import Button from "./Button";
+import {Link, Route} from "react-router-dom";
 
 function Header() {
     return (
         <div className="header">
             <div className="container">
-                <div className="header__logo">
-                    <img width="38" src={logoSvg} alt="Pizza logo"/>
-                    <div>
-                        <h1>React Pizza</h1>
-                        <p>самая вкусная пицца во вселенной</p>
+                <Link to="/">
+                    <div className="header__logo">
+                        <img width="38" src={logoSvg} alt="Pizza logo"/>
+                        <div>
+                            <h1>React Pizza</h1>
+                            <p>самая вкусная пицца во вселенной</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+
+                <Route path="/" exact>
                 <div className="header__cart">
-                    <Button className="button--cart"><span>520 ₽</span>
+                    <Link to="/cart">
+                        <Button className="button--cart"><span>520 ₽</span>
                         <div className="button__delimiter"></div>
                         <svg
                             width="18"
@@ -44,8 +50,11 @@ function Header() {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>3</span></Button>
+                        <span>3</span>
+                        </Button>
+                    </Link>
                 </div>
+                </Route>
             </div>
         </div>
     )
