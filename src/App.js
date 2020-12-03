3847b7ca41248fad258e7f3fcc68fc4cc64e600f
alex <alex_kobilianskiy@bigmir.net>
2020-12-03 21:36:33 +0200

    part 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 import {Header} from "./components";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import {Route} from "react-router-dom";
 
+
 function App() {
+
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+      axios.get('http://localhost:3000/db.json').then(({data}) => {
+          setPizzas(data.pizzas);
+      });
+  }, []);
+
   return (
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route component={Home} path="/" exact/>
+          <Route render={() => <Home items={pizzas}/>} path="/" exact/>
           <Route component={Cart} path="/cart" />
         </div>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,4 +12,6 @@ function Button(props) {
     )
 }
 
+
+
 export default Button;
